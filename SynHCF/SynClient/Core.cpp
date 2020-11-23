a2cd139de6f0277ed248ada96c09338aaf705d2f
Sutinder Saini <sutinder456@gmail.com>
2020-11-22 22:18:20 -0800

    Add synclient dll project

---
+++ SynHCF/SynClient/Core.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+DWORD WINAPI MainThread(LPVOID lpvoid) {
+
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
---
+++ SynHCF/SynClient/Helper.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Windows.h>
+
+HINSTANCE dllHandle;
+
+//void getprochandle();
+//void* getSysHandle();
